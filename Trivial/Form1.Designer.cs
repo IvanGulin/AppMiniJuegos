289ae46224208587cfeace5677c024a477918e4a
Iván Gulin <ivanga96@educastur.es>
2024-05-29 03:55:22 +0200

    Añadiendo cambios del forms como el icono y tamaño de la ventana

--- Trivial/Form1.Designer.cs
+++ Trivial/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.preguntaTexto = new System.Windows.Forms.Label();
             this.acertadas = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -39,98 +40,59 @@
             // 
             // preguntaTexto
             // 
-            this.preguntaTexto.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.preguntaTexto, "preguntaTexto");
             this.preguntaTexto.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.preguntaTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.preguntaTexto.Location = new System.Drawing.Point(280, 74);
             this.preguntaTexto.Name = "preguntaTexto";
-            this.preguntaTexto.Size = new System.Drawing.Size(469, 71);
-            this.preguntaTexto.TabIndex = 0;
-            this.preguntaTexto.Text = "label1";
-            this.preguntaTexto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // acertadas
             // 
-            this.acertadas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.acertadas, "acertadas");
             this.acertadas.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.acertadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.acertadas.Location = new System.Drawing.Point(25, 41);
             this.acertadas.Name = "acertadas";
-            this.acertadas.Size = new System.Drawing.Size(114, 80);
-            this.acertadas.TabIndex = 5;
-            this.acertadas.Text = "label1";
-            this.acertadas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.label6, "label6");
             this.label6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(25, 12);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(114, 29);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Aciertos";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.button1, "button1");
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(123, 212);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(307, 123);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.button2, "button2");
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(569, 212);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(307, 123);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.button3, "button3");
             this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(123, 382);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(307, 123);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.button4, "button4");
             this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button4.Location = new System.Drawing.Point(569, 382);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(307, 123);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1032, 538);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -139,7 +101,6 @@
             this.Controls.Add(this.preguntaTexto);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
